e8301f172dcf56a6f91b46e61d46e4da670e34b6
Steve Thompson <writetome51@gmail.com>
2018-10-03 01:31:50 -0600

    modified test file

--- tests/openArray_test.js
+++ tests/openArray_test.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var ObjectFactory_1 = require("@writetome51/object-factory/ObjectFactory");
 var OpenArray_1 = require("../OpenArray");
-var arr = ObjectFactory_1.ObjectFactory.getInstance(OpenArray_1.OpenArray);
+var arr = ObjectFactory_1.ObjectFactory.getInstance(OpenArray_1.OpenArray, [[1, 2, 3, 4, 5, 6, 7, 8, 9]]);
+console.log(arr);
 /***
  Example of using OpenArray
 
- let arr = ObjectFactory.getInstance(OpenArray, [[1,2,3,4,5,6,7,8,9]]);
+
  arr.remove.allAfterFirst(4);
  arr.remove.allBeforeFirst(3);
  arr.prepend.one(100);
